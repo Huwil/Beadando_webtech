c5c4cbb603aeb6b8ee7b04013565cd235f344ef8
irevai <revaipisti@hotmail.com>
2018-12-04 20:51:07 +0100

    Added new method.

--- student/js/addManufacturers.js
+++ student/js/addManufacturers.js
@@ -34,7 +34,7 @@ function loadManufacturers()
         $.each(data, function (key, value)
         {
             let sor = $('<tr></tr>');
-            let nevCella = $('<td onclick="openCar(' + "'" +value.name + "'" +')">' + value.name + '</td>');
+            let nevCella = $('<td onclick="CookieManufacturers(' + "'" +value.name + "'" +')">' + value.name + '</td>');
             let orszagCella = $('<td>' + value.country + '</td>');
             let alapitasCella = $('<td>' + value.founded + '</td>');
             sor.append(nevCella);
@@ -49,7 +49,35 @@ function loadManufacturers()
 }
 
 
-function openCar(manufacturer){
-    document.cookie="name="+manufacturer;
-    alert(manufacturer);
+function CookieManufacturers(manufacturer)
+{
+    document.cookie = "name=" + manufacturer;
+    $.getJSON
+    ("manufacturer", function (data)
+    {
+        $('#gyartoautoi').empty();
+        let table = $('<table class="table-black"></table>');
+        table.append('<tr><th>Név</th><th>Fogyasztás</th><th>Szín</th><th>Gyártó</th><th>Elérhetőség</th><th>Évjárat</th><th>Lóerő</th>');
+
+        $.each(data, function (key, value) {
+            let sor = $('<tr></tr>');
+            let nev = $('<td>' + value.name + '</td>');
+            let fogyasztas = $('<td>' + value.consumption + '</td>');
+            let szin = $('<td>' + value.color + '</td>');
+            let gyarto = $('<td>' + value.manufacturer + '</td>');
+            let elerhetoseg = $('<td>' + value.available + '</td>');
+            let evjarat = $('<td>' + value.year + '</td>');
+            let loero = $('<td>' + value.horsepower + '</td>');
+            sor.append(nev);
+            sor.append(fogyasztas);
+            sor.append(szin);
+            sor.append(gyarto);
+            sor.append(elerhetoseg);
+            sor.append(evjarat);
+            sor.append(loero);
+            table.append(sor);
+        })
+
+        $("#gyartoautoi").append(table);
+    })
 }
